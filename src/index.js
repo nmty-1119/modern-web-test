6e9e09dac9d3ac23824fdd87a0c7dccf8cf8ffca
daiki.nemoto <nmt1119.nmgw@gmail.com>
2020-03-18 17:29:51 +0900

    optimization.splitChunksでnode_modulesのバンドルを別ファイルとして出力

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { getHelloElement } from './sub';
+import jQuery from 'jquery';
+
+window.jQuery = jQuery;
+window.$ = jQuery;
 
 document.body.onload = function () {
   const newElm = getHelloElement();
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,60 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  watchOptions: {
-    ignored: '/node_modules/'
-  },
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    open: true,
-    port: 30000
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: /(node_modules|dist)/,
-        loader: 'eslint-loader',
-        options: {
-          fix: true
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-              ]
-            }
+module.exports = (env, argv) => {
+  return {
+    mode: argv.mode || 'development',
+    watchOptions: {
+      ignored: '/node_modules/'
+    },
+    entry: {
+      app: './src/index.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist/js')
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      open: true,
+      port: 30000
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /(node_modules|dist)/,
+          loader: 'eslint-loader',
+          options: {
+            fix: true
           }
-        ]
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            name: 'vendor',
+            chunks: 'initial',
+          }
+        }
       }
-    ]
-  },
-  devtool: 'source-map'
+    }
+  };
 };
